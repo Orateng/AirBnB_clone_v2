c21af4d4f4edc4bb012f96d616c5ccbad18e29d9
Orateng <1818.blackmail@gmail.com>
2023-10-23 12:26:25 -0700

    My file

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+
+from models import storage
+from flask import Flask,
+from flask import render_templates
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Closes the storage on teardown."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Start the Flask development server"""
+    app.run(host="0.0.0.0", port=5000)
